437554b84caf2df7aa6a90296913e80f2648d5b6
JacobBermudes <marriagesebers@gmail.com>
2026-02-26 19:03:29 +0500

    Routing to home page from vite env

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,7 @@ import Home from "./pages/Home";
 function Router() {
   return (
     <Switch>
-      <Route path="/" component={Home} />
+      <Route path={import.meta.env.BASE_URL} component={Home} />
       <Route component={NotFound} />
     </Switch>
   );
